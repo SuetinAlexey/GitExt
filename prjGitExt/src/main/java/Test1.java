25e12d58ef7c832bd9ecff9e14bbe7cac5d90497
Alexey Suetin <avsuetin@gmail.com>
2021-08-04 14:40:54 +0300

    База проекта

--- prjGitExt/src/main/java/Test1.java
+++ prjGitExt/src/main/java/Test1.java
@@ -2,7 +2,7 @@ public class Test1 {
 
     public static void main(String args[]){
         String s = "1";
-        s.concat("2");
+        s = s.concat("2");
         System.out.println(s);
     }
 }
